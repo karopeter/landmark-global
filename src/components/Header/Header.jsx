702efba77758016b33b35405f9768cb099ecdffc
devtochit <71229278+devtochit@users.noreply.github.com>
2024-07-06 15:22:03 +0100

    fezt:

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,26 @@
 import './Header.css';
-
+import heroImage from '../../assets/hero.jpg';
 const Header = () => {
   return (
     <>
       <div className='video-container h-[50vh]'>
-        <video autoPlay muted loop id='bg-video'>
+        <video
+          id='bg-video'
+          autoPlay
+          loop
+          muted
+          playsInline
+          className='desktop-video'
+        >
           <source
             src='https://landmarkglobal.com/uploads/videos/Landmark-Main-Short-Film-4-July-5.webm'
             type='video/webm'
           />
-          Your browser does not support the video tag.
         </video>
+        <img src={heroImage} alt='Hero' className='mobile-image' />
         <div className='text-overlay'>
-          <div className='content'>
-            <h1>Open Your World</h1>
+          <div className='content text-white'>
+            <h1 className='text-white'>Open Your World</h1>
             <p>
               The trusted international logistics partner,
               <br />
